7c8dfae83451774ae8b5399c10a0a26b8fc83bfb
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-08-22 15:53:01 +0800

    Add todo demo

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,42 +1,11 @@
 <script>
-  export let name;
+    import Todos from "./components/Todos.svelte"
 
-  function toggleName() {
-    if (name == "world") {
-      name = "Svelte";
-    } else {
-      name = "world";
-    }
-  }
+    let todos = [
+    { id: 1, name: "Create a Svelte starter app", completed: true },
+    { id: 2, name: "Create your first component", completed: true },
+    { id: 3, name: "Complete the rest of the tutorial", completed: false }
+  ];
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <button on:click={toggleName}>Toggle name</button>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<Todos {todos}/>
