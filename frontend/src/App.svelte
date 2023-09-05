3b69808ac8fefbaa9d7128764303a1b18a773638
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-09-05 17:18:34 +0800

    Cleanup

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,4 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import Stories from './lib/Stories.svelte';
 </script>
 
