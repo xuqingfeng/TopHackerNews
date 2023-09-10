f0cd4776088a3b5ad09924643726d1d22f518678
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-09-10 22:35:45 +0800

    Add apollo/client

--- web/src/lib/Stories.svelte
+++ web/src/lib/Stories.svelte
@@ -1,53 +1,57 @@
 <script>
+  import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
+
   let stories = [];
-  const topStoriesQuery = `
-    query topStories {
-      topStories(offset: 0, limit: 10) {
-        id
-        kids
-        score
-        time
-        title
-        type
-        url
-        by
-      }
-    }
-  `;
-  fetch("http://localhost:8080/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ query: topStoriesQuery }),
-  })
-    .then((r) => r.json())
-    .then((data) => (stories = data.data.topStories));
+
+  const client = new ApolloClient({
+    uri: "http://localhost:8080/graphql",
+    cache: new InMemoryCache(),
+  });
+  client
+    .query({
+      query: gql`
+        query topStories {
+          topStories(offset: 0, limit: 10) {
+            id
+            kids
+            score
+            time
+            title
+            type
+            url
+            by
+          }
+        }
+      `,
+    })
+    .then((data) => {
+      stories = data.data.topStories;
+    });
 </script>
 
 <div>
-  <!-- {#if $stories.loading}
-  <div></div>
-  {:else if $stories.error}
-  <div>Error!</div>
-  {:else if $stories} -->
-  <ul>
-    {#each stories as story (story.id)}
-      <li>
-        <div>
-          <p>
-            {story.title} - <a class="" href={story.url}>{story.url}</a>
-          </p>
-        </div>
-        <div>
-          <p>
-            By: {story.by}
-            <time datetime="">Time: {story.time}</time>
-          </p>
-        </div>
-      </li>
-    {/each}
-  </ul>
-  <!-- {/if} -->
+  {#if stories.length == 0}
+    <div class="alert alert-info">
+      <span class="loading" />
+      Loading...
+    </div>
+  {:else}
+    <ul>
+      {#each stories as story (story.id)}
+        <li>
+          <div>
+            <p>
+              {story.title} - <a class="" href={story.url}>{story.url}</a>
+            </p>
+          </div>
+          <div>
+            <p>
+              By: {story.by}
+              <time datetime="">Time: {story.time}</time>
+            </p>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  {/if}
 </div>
