eab6b863265b3cb8530fe9531247e7734e81d350
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-11-23 15:10:52 +0800

    Store theme in local storage, fix #6

--- web/src/lib/Header.svelte
+++ web/src/lib/Header.svelte
@@ -1,5 +1,8 @@
 <script>
   const WEB_URL = import.meta.env.VITE_WEB_URL;
+  const THEME_NAME = "theme";
+  const storedTheme = localStorage.getItem(THEME_NAME);
+  changeTheme(storedTheme);
 
   // TODO: find a native 'svelte' way to handle this
   function changeTheme(theme) {
@@ -9,30 +12,40 @@
         document.body.classList.remove("dark-grey");
         document.body.classList.remove("solarized-dark");
         document.body.classList.remove("standard");
+
+        localStorage.setItem(THEME_NAME, theme);
         break;
       case "dark":
         document.body.classList.add("dark");
         document.body.classList.remove("dark-grey");
         document.body.classList.remove("solarized-dark");
         document.body.classList.remove("standard");
+
+        localStorage.setItem(THEME_NAME, theme);
         break;
       case "dark-grey":
         document.body.classList.add("dark-grey");
         document.body.classList.remove("dark");
         document.body.classList.remove("solarized-dark");
         document.body.classList.remove("standard");
+
+        localStorage.setItem(THEME_NAME, theme);
         break;
       case "solarized-dark":
         document.body.classList.add("solarized-dark");
         document.body.classList.remove("dark");
         document.body.classList.remove("dark-grey");
         document.body.classList.remove("standard");
+
+        localStorage.setItem(THEME_NAME, theme);
         break;
       default:
         document.body.classList.remove("dark");
         document.body.classList.remove("dark-grey");
         document.body.classList.remove("solarized-dark");
         document.body.classList.remove("standard");
+
+        localStorage.setItem(THEME_NAME, "light");
     }
   }
 </script>
